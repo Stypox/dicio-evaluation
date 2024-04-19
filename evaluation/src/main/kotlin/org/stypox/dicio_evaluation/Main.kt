568d2daa688fc27fc72e4566214b9171660ed52d
Stypox <stypox@pm.me>
2024-04-19 08:49:46 +0200

    Add more functions

--- evaluation/src/main/kotlin/org/stypox/dicio_evaluation/Main.kt
+++ evaluation/src/main/kotlin/org/stypox/dicio_evaluation/Main.kt
@@ -1,4 +1,5 @@
 package org.stypox.dicio_evaluation
+import kotlin.math.min
 import kotlin.math.pow
 
 const val SCORING_F_PARAM = 0.9
@@ -11,6 +12,17 @@ fun scoringF(stats: Stats): Double {
     return (stats.userMatched + stats.refMatched) / denominator.toDouble().pow(SCORING_F_PARAM)
 }
 
+const val SCORING_G_UM = 2.0
+const val SCORING_G_UW = -1.0
+const val SCORING_G_RM = 2.0
+const val SCORING_G_RW = -1.0
+fun scoringG(stats: Stats): Double {
+    return SCORING_G_UM * stats.userMatched +
+            SCORING_G_UW * stats.userWeight +
+            SCORING_G_RM * stats.refMatched +
+            SCORING_G_RW * stats.refWeight
+}
+
 fun pruningNone(scoringFunction: (stats: Stats) -> Double, options: MutableList<Stats>) {
 }
 
@@ -20,10 +32,17 @@ fun pruningBestScore(scoringFunction: (stats: Stats) -> Double, options: Mutable
     options.add(best)
 }
 
+fun pruningBestHalfScore(scoringFunction: (stats: Stats) -> Double, options: MutableList<Stats>) {
+    options.sortByDescending(scoringFunction)
+    for (i in 0..min(options.size - 16, options.size / 2)) {
+        options.removeLast()
+    }
+}
+
 fun main() {
     val matcher = Matcher(
-        scoringFunction = ::scoringF,
-        pruningFunction = ::pruningNone,
+        scoringFunction = ::scoringG,
+        pruningFunction = ::pruningBestScore,
         userWords = listOf("a", "b", "c", "e", "d", "g", "c", "d", "e"),
         refWords = listOf("a", "c", "d", "g", "e", "c", "d", "g", "e"),
     )
