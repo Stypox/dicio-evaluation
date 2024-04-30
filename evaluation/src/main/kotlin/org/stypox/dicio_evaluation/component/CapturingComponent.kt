c99161e02a8a518bf63b681fbd26823ab6d098c6
Stypox <stypox@pm.me>
2024-04-30 10:55:59 +0200

    Use cumulativeWeight in CapturingComponent

--- evaluation/src/main/kotlin/org/stypox/dicio_evaluation/component/CapturingComponent.kt
+++ evaluation/src/main/kotlin/org/stypox/dicio_evaluation/component/CapturingComponent.kt
@@ -1,14 +1,18 @@
 package org.stypox.dicio_evaluation.component
 
 import org.stypox.dicio_evaluation.context.MatchContext
+import org.stypox.dicio_evaluation.context.cumulativeWeight
 
 class CapturingComponent(
     private val weight: Float
 ) : Component() {
     override fun match(start: Int, end: Int, ctx: MatchContext): List<MatchResult> {
+        val cumulativeWeight = ctx.getOrTokenize("cumulativeWeight", ::cumulativeWeight)
+        val userWeight = cumulativeWeight[end] - cumulativeWeight[start]
+
         return listOf(MatchResult(
-            userMatched = (end-start) / 10.0f,
-            userWeight = (end-start) / 10.0f,
+            userMatched = userWeight,
+            userWeight = userWeight,
             refMatched = if (start == end) 0.0f else weight,
             refWeight = weight,
             end = end,
